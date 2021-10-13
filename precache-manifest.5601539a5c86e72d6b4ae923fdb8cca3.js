06fc8017c681b7d2e296bf06de090ed3bf2e7a60
ThomasRudynski <thomas.rudynski@gmail.com>
2021-10-13 18:08:10 +0200

    Updates

---
+++ precache-manifest.5601539a5c86e72d6b4ae923fdb8cca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293524f560a06077de4f712eec53b40b",
+    "url": "/AlyraGHP/whitelist-system-with-heroku-master/index.html"
+  },
+  {
+    "revision": "968e0bc2e825468e67b3",
+    "url": "/AlyraGHP/whitelist-system-with-heroku-master/static/css/2.9ff99f11.chunk.css"
+  },
+  {
+    "revision": "e86afcb0c49a29be6fdb",
+    "url": "/AlyraGHP/whitelist-system-with-heroku-master/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "968e0bc2e825468e67b3",
+    "url": "/AlyraGHP/whitelist-system-with-heroku-master/static/js/2.9bc8a53e.chunk.js"
+  },
+  {
+    "revision": "e86afcb0c49a29be6fdb",
+    "url": "/AlyraGHP/whitelist-system-with-heroku-master/static/js/main.ed72b7cd.chunk.js"
+  },
+  {
+    "revision": "80c4fae6c383cecfa216",
+    "url": "/AlyraGHP/whitelist-system-with-heroku-master/static/js/runtime-main.4d69fafa.js"
+  }
+]);
